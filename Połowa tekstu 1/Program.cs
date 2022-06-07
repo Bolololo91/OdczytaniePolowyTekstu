3dddcdca8f6b3bb8795cba04cc87bcbd768b0e89
Bolololo91 <karolczarnecki5@gmail.com>
2022-06-07 12:04:07 +0200

    Dodaj pliki projektów.

---
+++ Połowa tekstu 1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Połowa_tekstu_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wpisz tekst");
+            string tekst = Console.ReadLine();
+            char[] karol = tekst.ToArray();
+            string newKarol = "";
+            for (int i = 0; i <= tekst.Length-1; i++)
+            {
+                if (i>(tekst.Length-1)/2)
+                {
+                    newKarol += karol[i];
+
+                }
+            }Console.Write(newKarol);
+            Console.ReadKey();
+        }
+    }
+}
